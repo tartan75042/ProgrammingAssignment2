b24524a3503ae17a3fc9db6fdf2b504c97a3abbc
Marissa Kuhn <mk012c@yahoo.com>
2015-07-25 12:18:57 -0400

    Explanations of Functions

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,16 @@
 ## It is useful to cache because matrix inversion is usually a costly computation and
 ## resources can be saved through cache rather than computing it repeatedly.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix 
+## above.  If the inverse has already been calculated (and the matrix has not changed), then
+## the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
